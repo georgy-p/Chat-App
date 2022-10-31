dabbff63393dee98b6dc61fc39bf8a936f91c75a
George <pigargeorge@gmail.com>
2022-10-31 19:16:27 +0300

    add router

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+} from 'react-router-dom';
+import ErrorPage from '../routes/error-page.jsx';
+import LoginPage from './Login.jsx';
 
-function App() {
-  return (
-    <div>Hello, World</div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: null,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'login',
+    element: <LoginPage />,
+  },
+]);
+
+const App = () => (
+  <RouterProvider router={router} />
+);
 
 export default App;
