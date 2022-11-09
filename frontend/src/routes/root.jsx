d25573c31e12eee70c6491cd04e4efb04a2f9f33
George <pigargeorge@gmail.com>
2022-11-09 17:07:44 +0300

    add basic components and style

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,3 +1,15 @@
-const Root = () => <div>Hello, World</div>;
+import { Navbar, Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => (
+  <div className="d-flex flex-column h-100">
+    <Navbar bg="light" expand="lg" className="shadow-sm">
+      <Container>
+        <Navbar.Brand>SUPER Chat</Navbar.Brand>
+      </Container>
+    </Navbar>
+    <Outlet />
+  </div>
+);
 
 export default Root;
