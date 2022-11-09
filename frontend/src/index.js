555dd97d610aa05def80b00e328f668c83a4b5f3
George <pigargeorge@gmail.com>
2022-11-09 17:07:08 +0300

    connect bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const mountNode = document.getElementById('chat');
 const root = ReactDOM.createRoot(mountNode);
