7d6a2778d8d03e7a4f67d197638cb2d12e835c9b
George <pigargeorge@gmail.com>
2022-11-12 13:58:13 +0300

    add AuthProvider and Routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import {
-  createBrowserRouter,
-  RouterProvider,
-  // Route,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
 } from 'react-router-dom';
-import Root from '../routes/root.jsx';
-import ErrorPage from '../routes/error-page.jsx';
-import LoginPage from './Login.jsx';
+import Layout from '../routes/layout.jsx';
+// import ErrorPage from '../routes/error-page.jsx';
+import LoginPage from './LoginPage.jsx';
+import AuthContext from '../contexts/index.jsx';
+import useAuth from '../hooks/index.jsx';
+import PrivatePage from '../routes/private.jsx';
+// import useAuth from '../hooks/index.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'login',
-        element: <LoginPage />,
-      },
-    ],
-  },
-]);
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  const logFuncs = useMemo(() => ({ loggedIn, logIn, logOut }), [loggedIn]);
+
+  return (
+    <AuthContext.Provider value={logFuncs}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
 
 const App = () => (
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <Routes element={<Layout />}>
+      <Route
+        path="/"
+        element={(
+          <PrivateRoute>
+            <PrivatePage />
+          </PrivateRoute>
+          )}
+      />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </AuthProvider>
 );
 
 export default App;
