b2855e4c0c157461028a7e55ac637514a46c6fd2
George <pigargeorge@gmail.com>
2022-11-16 11:23:53 +0300

    add div element

--- frontend/src/routes/layout.jsx
+++ frontend/src/routes/layout.jsx
@@ -2,13 +2,17 @@ import { Navbar, Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => (
-  <div className="d-flex flex-column h-100">
-    <Navbar bg="light" expand="lg" className="shadow-sm">
-      <Container>
-        <Navbar.Brand>SUPER Chat</Navbar.Brand>
-      </Container>
-    </Navbar>
-    <Outlet />
+  <div className="h-100 bg-light">
+    <div className="d-flex flex-column h-100">
+      <Navbar bg="light" expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand>
+            SUPER Chat
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
   </div>
 );
 
