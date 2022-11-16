821a9e8830caba0c8e3fdd8d56c4adf91093f3f3
George <pigargeorge@gmail.com>
2022-11-16 11:20:49 +0300

    add redux provider and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './slices/index.js';
 
 const mountNode = document.getElementById('chat');
 const root = ReactDOM.createRoot(mountNode);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 );
