54e35850fad6d45e6361efc69559c10787c106f7
George <pigargeorge@gmail.com>
2022-12-06 14:52:05 +0300

    add signout button and link to root page

--- frontend/src/routes/layout.jsx
+++ frontend/src/routes/layout.jsx
@@ -1,19 +1,27 @@
-import { Navbar, Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
+import { Navbar, Container, Button } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.jsx';
 
-const Layout = () => (
-  <div className="h-100 bg-light">
-    <div className="d-flex flex-column h-100">
-      <Navbar bg="light" expand="lg" className="shadow-sm">
-        <Container>
-          <Navbar.Brand>
-            SUPER Chat
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <Outlet />
+const Layout = () => {
+  const auth = useAuth();
+
+  const handleClick = () => auth.logOut();
+
+  return (
+    <div className="h-100 bg-light">
+      <div className="d-flex flex-column h-100">
+        <Navbar bg="light" expand="lg" className="shadow-sm">
+          <Container>
+            <Link className="navbar-brand" to="/">
+              Hexlet Chat
+            </Link>
+            {auth.loggedIn ? <Button onClick={handleClick}>Выйти</Button> : null}
+          </Container>
+        </Navbar>
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
