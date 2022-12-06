f270e264603579ffaf74ad19f90536a4c033681f
George <pigargeorge@gmail.com>
2022-12-06 10:43:40 +0300

    add modal window for channels renaming

---
+++ frontend/src/components/chat/modals/Rename.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { useFormik } from 'formik';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as yup from 'yup';
+import _ from 'lodash';
+import { io } from 'socket.io-client';
+import {
+  Button, Form, Modal,
+} from 'react-bootstrap';
+import { chSelectors } from '../../../slices/channelsSlice.js';
+import { actions as modalsActions } from '../../../slices/modalsSlice.js';
+
+const socket = io();
+
+const Add = () => {
+  const dispatch = useDispatch();
+  const handleClose = () => dispatch(modalsActions.closeModal());
+  const channelId = useSelector((state) => state.modals.channelId);
+  const channel = useSelector((state) => chSelectors.selectById(state, channelId));
+  const [renameFailed, setRenameFailed] = useState(false);
+
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const channelsNames = useSelector(chSelectors.selectAll).map((ch) => ch.name);
+  const formik = useFormik({
+    initialValues: {
+      body: channel.name,
+    },
+    validationSchema: yup.object({
+      body: yup.string().required(),
+    }),
+    onSubmit: (values) => {
+      if (_.includes(channelsNames, values.body)) {
+        setRenameFailed(true);
+      } else {
+        socket.emit('renameChannel', { id: channelId, name: values.body });
+        handleClose();
+      }
+    },
+  });
+
+  return (
+    <Modal show onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Переименовать канал</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group>
+            <Form.Control
+              required
+              ref={inputRef}
+              name="body"
+              type="text"
+              className="mb-2"
+              isInvalid={renameFailed}
+              {...formik.getFieldProps('body')}
+            />
+            <Form.Control.Feedback type="invalid">Должно быть уникальным</Form.Control.Feedback>
+            <div className="d-flex justify-content-end">
+              <Button variant="secondary me-2" onClick={handleClose}>
+                Отменить
+              </Button>
+              <Button type="submit" variant="primary">
+                Отправить
+              </Button>
+            </div>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default Add;
