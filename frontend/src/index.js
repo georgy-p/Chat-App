1054ec453daf33e14710fb10175c11a47085d282
George <pigargeorge@gmail.com>
2023-02-15 20:32:18 +0300

    add RollbarProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as RollbarProvider } from '@rollbar/react';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n.js';
@@ -10,12 +11,20 @@ import store from './slices/index.js';
 
 const mountNode = document.getElementById('chat');
 const root = ReactDOM.createRoot(mountNode);
+
+const rollbarConfig = {
+  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
+  environment: 'production',
+};
+
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
+  <RollbarProvider config={rollbarConfig}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  </RollbarProvider>,
 );
